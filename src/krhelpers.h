870a796f395f6b249633fd5651499ecd091c99c3
kearwood <kearwood@kearwood.com>
2022-07-07 23:20:01 -0700

    Removed Kraken Engine Specific GL code from Hydra. Removed krhelpers.cpp, as it only contained GL code which is no longer used.

--- src/krhelpers.h
+++ src/krhelpers.h
@@ -31,21 +31,6 @@
 
 #pragma once
 
-#if defined(KRAKEN_USE_OPENGL)
-
-#if defined(_WIN32) || defined(_WIN64)
-#include <GL/glew.h>
-#elif defined(__linux__) || defined(__unix__) || defined(__posix__)
-#include <GL/gl.h>
-#include <GL/glu.h>
-#include <GL/glext.h>
-#elif defined(__APPLE__)
-#include <OpenGL/gl3.h>
-#include <OpenGL/gl3ext.h>
-#endif
-
-#endif // defined(KRAKEN_USE_OPENGL)
-
 #include "../include/hydra.h"
 
 #define KRMIN(x,y) ((x) < (y) ? (x) : (y))
@@ -55,12 +40,3 @@
 
 float const PI = 3.141592653589793f;
 float const D2R = PI * 2 / 360;
-
-#if defined(KRAKEN_USE_OPENGL)
-namespace kraken {
-  void SetUniform(GLint location, const Vector2 &v);
-  void SetUniform(GLint location, const Vector3 &v);
-  void SetUniform(GLint location, const Vector4 &v);
-  void SetUniform(GLint location, const Matrix4 &v);
-} // namespace kraken
-#endif // defined(KRAKEN_USE_OPENGL)
